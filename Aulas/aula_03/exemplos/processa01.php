b91e78dba2f4f943520dc396ca94a91432443865
Thiago S. Santos <160339863+xk0-thg93@users.noreply.github.com>
2025-02-27 11:10:17 -0300

    Add files via upload

---
+++ Aulas/aula_03/exemplos/processa01.php
@@ -0,0 +1,8 @@
+<?php
+    $nome = $_POST['boxnome'];
+    $idade = $_POST['boxidade'];
+
+    echo "Nome digitado: $nome <br>";
+    echo "Idade digitada: $idade <br>";
+
+?>
---
+++ Aulas/aula_03/exercicio/processamento.php
@@ -0,0 +1,9 @@
+<?php
+
+    $nome = $_POST['nombre'];
+    $message = $_POST['mensagem'];
+
+    echo"O nome digitado foi: $nome <br>";
+    echo"E a mensagem escrita por $nome foi: $message";
+?>
+
